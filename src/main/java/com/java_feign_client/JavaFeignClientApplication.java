7426bd634694ffa124b26dd5d12d386d0efcff37
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-08-14 17:49:09 -0600

    starting new project java_feign_client

---
+++ src/main/java/com/java_feign_client/JavaFeignClientApplication.java
@@ -0,0 +1,13 @@
+package com.java_feign_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFeignClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFeignClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java_feign_client/JavaFeignClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_feign_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFeignClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
