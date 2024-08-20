855ba9531295a5ee94bf3c611a77072614f8b8b2
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-08-20 15:09:05 -0600

    implement interface Feign

--- src/main/java/com/java_feign_client/GitHubClient.java
+++ src/main/java/com/java_feign_client/GitHubClient.java
@@ -1,4 +1,13 @@
 package com.java_feign_client;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "githubClient", url = "https://api.github.com")
 public interface GitHubClient {
+
+  @GetMapping("/repos/{owner}/{repo}")
+  Repository getRepository(@PathVariable String owner, @PathVariable String repo);
+
 }
