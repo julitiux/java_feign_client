fac795d5b62035702551203b1388b8fc93af786d
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-08-20 15:13:12 -0600

    implement service GitHubService

--- src/main/java/com/java_feign_client/GitHubService.java
+++ src/main/java/com/java_feign_client/GitHubService.java
@@ -1,4 +1,18 @@
 package com.java_feign_client;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class GitHubService {
+
+  private final GitHubClient gitHubClient;
+
+  public GitHubService(GitHubClient gitHubClient) {
+    this.gitHubClient = gitHubClient;
+  }
+
+  public Repository getRepository(String owner, String repo){
+    return gitHubClient.getRepository(owner, repo);
+  }
+
 }
