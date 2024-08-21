fcf836b615598ccf9d4503fef0bccdee04618e2c
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-08-20 18:55:49 -0600

    implement controller with get request info_repo

--- src/main/java/com/java_feign_client/GitHubController.java
+++ src/main/java/com/java_feign_client/GitHubController.java
@@ -1,4 +1,25 @@
 package com.java_feign_client;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
 public class GitHubController {
+
+  private final GitHubService gitHubService;
+
+  public GitHubController(GitHubService gitHubService) {
+    this.gitHubService = gitHubService;
+  }
+
+  @GetMapping("info_repo/{owner}/{repo}")
+  public Repository getRepository(@PathVariable String owner, @PathVariable String repo){
+    return gitHubService.getRepository(owner, repo);
+  }
+
 }
