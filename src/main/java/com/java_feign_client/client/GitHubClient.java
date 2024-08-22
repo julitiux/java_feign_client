badc72530f0eb8f99d08bc7d187df8e98f528363
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-08-21 23:38:10 -0600

    add new mapping to get user from username

--- src/main/java/com/java_feign_client/client/GitHubClient.java
+++ src/main/java/com/java_feign_client/client/GitHubClient.java
@@ -1,6 +1,7 @@
 package com.java_feign_client.client;
 
 import com.java_feign_client.model.Repository;
+import com.java_feign_client.model.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,5 +11,8 @@ public interface GitHubClient {
 
   @GetMapping("/repos/{owner}/{repo}")
   Repository getRepository(@PathVariable String owner, @PathVariable String repo);
+
+  @GetMapping("/users/{username}")
+  User getUser(@PathVariable("username") String username);
 
 }
