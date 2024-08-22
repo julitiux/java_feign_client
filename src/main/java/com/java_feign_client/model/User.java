bba28725de3042f3357c0a122de1409fa3e6756a
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-08-21 19:56:09 -0600

    add User properties

--- src/main/java/com/java_feign_client/model/User.java
+++ src/main/java/com/java_feign_client/model/User.java
@@ -1,4 +1,23 @@
 package com.java_feign_client.model;
 
+import lombok.Data;
+
+@Data
 public class User {
+
+  private String login;
+  private Long id;
+  private String avatarUrl;
+  private String url;
+  private String htmlUrl;
+  private String name;
+  private String company;
+  private String blog;
+  private String location;
+  private String email;
+  private String bio;
+  private Integer publicRepos;
+  private Integer followers;
+  private Integer following;
+
 }
