d2775b206abaa597f7a06b9700e85f03a574754b
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-08-23 12:48:44 -0600

    complement properties Repository class

--- src/main/java/com/java_feign_client/model/Repository.java
+++ src/main/java/com/java_feign_client/model/Repository.java
@@ -5,8 +5,24 @@ import lombok.Data;
 @Data
 public class Repository {
 
+  private Long id;
+  private String nodeId;
   private String name;
+  private String fullName;
+  private Owner owner;
+  private String htmlUrl;
   private String description;
+  private Boolean fork;
   private String url;
+  private String archiveUrl;
+  private String assigneesUrl;
+  private String blobsUrl;
+  private String branchesUrl;
+  private String collaboratorsUrl;
+  private String commentsUrl;
+  private String commitsUrl;
+  private String compareUrl;
+  private String contentsUrl;
+  private String contributorsUrl;
 
 }
