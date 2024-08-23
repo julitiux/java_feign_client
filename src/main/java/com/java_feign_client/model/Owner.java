4f91704cec5c49f49387203a0412b047a8f5499d
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-08-23 12:48:56 -0600

    add Owner mapper

---
+++ src/main/java/com/java_feign_client/model/Owner.java
@@ -0,0 +1,30 @@
+package com.java_feign_client.model;
+
+import lombok.Data;
+
+@Data
+public class Owner {
+
+  private String name;
+  private String email;
+  private String login;
+  private Long id;
+  private String nodeId;
+  private String avatarUrl;
+  private String gravatarId;
+  private String url;
+  private String htmlUrl;
+  private String followersUrl;
+  private String followingUrl;
+  private String gistsUrl;
+  private String starredUrl;
+  private String subscriptionsUrl;
+  private String organizationsUrl;
+  private String reposUrl;
+  private String eventsUrl;
+  private String receivedEventsUrl;
+  private String type;
+  private String siteAdmin;
+  private String starredAt;
+
+}
